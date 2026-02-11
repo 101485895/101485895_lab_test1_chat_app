3ba76a5114f2f8efe81dd39aa9b2fdb82ee0d24b
101485895 <101485895@georgebrown.ca>
2026-02-11 16:59:54 -0500

    created schemas

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdon: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.index({ username: 1 }, { unique: true });
+module.exports = mongoose.model("User", userSchema);
