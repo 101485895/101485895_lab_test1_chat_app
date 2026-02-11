4c8cf2b01979cbf69e8ab612a01d419a33d17391
101485895 <101485895@georgebrown.ca>
2026-02-11 16:17:01 -0500

    basic server running and project structure setup

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
+app.use("/view", express.static(path.join(__dirname, "..", "view")));
+
+app.get("/", (req, res) => res.send("testing testing"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err.message));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
